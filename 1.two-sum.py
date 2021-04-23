11f2f74920c4b2e0feff63b33ff242162540781e
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-04-23 23:13:19 +0530

    Added: Two Sum

---
+++ 1.two-sum.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i,v in enumerate(nums):
+            if target - v in map:
+                return [map[target-v],i]
+            else:
+                map[v] = i 
+# @lc code=end
+
