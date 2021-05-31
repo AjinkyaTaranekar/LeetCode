466cd0f2d262bb3533b0318a1db76b4afb913443
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-05-31 21:06:20 +0530

    Added: Majority Element

---
+++ 169.majority-element.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=169 lang=python3
+#
+# [169] Majority Element
+#
+
+# @lc code=start
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        candidate, count = nums[0], 0
+        for num in nums:
+            if num == candidate:
+                count += 1
+            elif count == 0:
+                candidate, count = num, 1
+            else:
+                count -= 1
+        return candidate
+# @lc code=end
+
