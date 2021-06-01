243f1590910154c4a558c56b1080cc620f51a295
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-01 13:01:40 +0530

    Added: Counting Bits

---
+++ 338.counting-bits.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=338 lang=python3
+#
+# [338] Counting Bits
+#
+
+# @lc code=start
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = []
+        for i in range(n+1):
+            res.append(bin(i).count('1'))
+        return res
+# @lc code=end
+
