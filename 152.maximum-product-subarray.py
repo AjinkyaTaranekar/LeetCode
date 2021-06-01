863cb23abc5013e6a129f6445ff11df51f256da5
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-01 12:26:04 +0530

    Added: Maximum Product Subarray

---
+++ 152.maximum-product-subarray.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=152 lang=python3
+#
+# [152] Maximum Product Subarray
+#
+
+# @lc code=start
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        max_prod, min_prod, ans = nums[0], nums[0], nums[0]
+        for i in range(1, len(nums)):
+            x = max(nums[i], max_prod*nums[i], min_prod*nums[i])
+            y = min(nums[i], max_prod*nums[i], min_prod*nums[i])            
+            max_prod, min_prod = x, y
+            ans = max(max_prod, ans)
+        return ans
+# @lc code=end
+
