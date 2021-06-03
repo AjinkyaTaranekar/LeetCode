4b201d08bf61706a9693ae28d25a7659e78f84a5
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-03 13:45:53 +0530

    Added: Ugly Number

---
+++ 263.ugly-number.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=263 lang=python3
+#
+# [263] Ugly Number
+#
+
+# @lc code=start
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        for p in 2, 3, 5:
+            while n % p == 0 < n:
+                n //= p
+        return n == 1
+# @lc code=end
+
