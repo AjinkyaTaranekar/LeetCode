1aa8b382b7006d2de83e3f74390a18e47ebafda3
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-03 10:30:55 +0530

    Added: Subsets II

---
+++ 90.subsets-ii.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=90 lang=python3
+#
+# [90] Subsets II
+#
+
+# @lc code=start
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        nums.sort()
+        for i in range(len(nums)):
+            if i == 0 or nums[i] != nums[i - 1]:
+                l = len(res)
+            for j in range(len(res) - l, len(res)):
+                res.append(res[j] + [nums[i]])
+        return res
+
+# @lc code=end
+
