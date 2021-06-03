7d24dc1b4cccd29d36179fe5fd35735d1298b4ac
Ajinkya Taranekar <ajinkyataranekar@gmail.com>
2021-06-03 10:06:51 +0530

    Added: Unique Binary Search Trees

---
+++ 96.unique-binary-search-trees.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=96 lang=python3
+#
+# [96] Unique Binary Search Trees
+#
+
+# @lc code=start
+class Solution:
+    def numTrees(self, n: int) -> int:
+        return math.factorial(2*n)//(math.factorial(n)*math.factorial(n+1))
+# @lc code=end
+
